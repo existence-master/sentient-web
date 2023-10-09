cf6b0d5800c6332265e0764abcd060e1f7dbe347
SK <sarthakkarandikar03@gmail.com>
2023-10-09 13:42:07 +0530

    init mvp

---
+++ app/enter/page.js
@@ -0,0 +1,12 @@
+import Nav from "@components/Nav"
+import { Fragment } from "react"
+
+const Warning = () => {
+    return (
+        <Fragment>
+            <Nav action="none" />
+            <div></div>
+        </Fragment>
+        
+    )
+}
