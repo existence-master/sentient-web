32f7682fa951600bf96c3be8b8b72de10f0c89d3
SK <sarthakkarandikar03@gmail.com>
2023-10-16 21:04:53 +0530

    streamlit init

---
+++ interface/utils.py
@@ -0,0 +1,29 @@
+from langchain.llms import CTransformers
+from langchain.vectorstores import FAISS
+from langchain.memory import ConversationBufferMemory
+from langchain.embeddings import HuggingFaceEmbeddings
+from langchain.chains import ConversationalRetrievalChain
+from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain.document_loaders import PyPDFLoader, DirectoryLoader
+
+def load_documents():
+    loader = DirectoryLoader('data/', glob="*.pdf", loader_cls=PyPDFLoader)
+    documents = loader.load()
+    return documents
+
+def split_text_into_chunks(documents):
+    text_splitter = RecursiveCharacterTextSplitter(chunk_size=500, chunk_overlap=50)
+    text_chunks = text_splitter.split_documents(documents)
+    return text_chunks
+
+def create_embeddings():
+    embeddings = HuggingFaceEmbeddings(model_name="sentence-transformers/all-MiniLM-L6-v2", model_kwargs={'device': "cuda"})
+    return embeddings
+
+def create_vector_store(text_chunks, embeddings):
+    vector_store = FAISS.from_documents(text_chunks, embeddings)
+    return vector_store
+
+def create_llms_model():
+    llm = CTransformers(model="TheBloke/Mistral-7B-v0.1-GGUF", config={'max_new_tokens': 128, 'temperature': 0.01})
+    return llm
