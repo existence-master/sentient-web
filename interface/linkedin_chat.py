2374df22e24969ff630201c82252769abf863506
SK <sarthakkarandikar03@gmail.com>
2023-11-11 13:13:27 +0000

    Nov 11, 2023, 6:43 PM

--- interface/linkedin_chat.py
+++ interface/linkedin_chat.py
@@ -16,7 +16,8 @@ def app():
     with st.sidebar:
         
         def on_page_change(key) :
-            if key == "Chat"
+            if key == "Chat" :
+                
         menu = option_menu(None, ["Chat", "LinkedIn Advice", "Settings"], icons=["chat-fill", "linkedin", "gear"], menu_icon = "cast", default_index = 0, on_change = on_page_change)
         menu
 
