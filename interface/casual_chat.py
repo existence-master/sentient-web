afba528c98dfd780a8701305a3369ff1f5147de1
SK <sarthakkarandikar03@gmail.com>
2023-11-11 14:30:45 +0000

    Nov 11, 2023, 8:00 PM

--- interface/casual_chat.py
+++ interface/casual_chat.py
@@ -37,17 +37,7 @@ def app():
                 st.session_state.runpage = settings.app
 
             elif current_page == "Logout" :
-                for root, dirs, files in os.walk(f"interface/{st.session_state.username}", topdown = False):
-                    for name in files:
-                        os.remove(os.path.join(root, name))
-                    for name in dirs:
-                        os.rmdir(os.path.join(root, name))
-
-                os.rmdir(f"interface/{st.session_state.username}")
-                requests.post(f"{st.session_state.url}/terminate")
-
-                for key in st.session_state.keys():
-                    del st.session_state[key]
+               
 
             else :
                 pass
