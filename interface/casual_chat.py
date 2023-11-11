b6aa38cb5d18fcb61286909e5bf76309eb4b50ac
SK <sarthakkarandikar03@gmail.com>
2023-11-11 13:50:26 +0000

    Nov 11, 2023, 7:20 PM

--- interface/casual_chat.py
+++ interface/casual_chat.py
@@ -42,7 +42,7 @@ def app():
             else :
                 pass
 
-        menu = option_menu(None, ["Chat", "LinkedIn Advice", "Settings", "Logout"], icons=["chat-fill", "linkedin", "gear", "box-arrow-in-left"], menu_icon = "cast", default_index = 0, on_change = on_page_change, key =)
+        menu = option_menu(None, ["Chat", "LinkedIn Advice", "Settings", "Logout"], icons=["chat-fill", "linkedin", "gear", "box-arrow-in-left"], menu_icon = "cast", default_index = 0, on_change = on_page_change, key = "menu")
         menu      
        
     ai_container = st.container()
