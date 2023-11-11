69d717d86ec5ad1457154f7b0dad21344a52bf08
SK <sarthakkarandikar03@gmail.com>
2023-11-11 13:17:45 +0000

    Nov 11, 2023, 6:47 PM

--- interface/linkedin_chat.py
+++ interface/linkedin_chat.py
@@ -25,7 +25,7 @@ def app():
                 st.session_state.runpage = settings.app
                 st.rerun()
             elif key == "Logout" :
-                
+
             else :
                 pass
 
@@ -33,17 +33,7 @@ def app():
         menu
 
         if st.button("Logout"):
-            for root, dirs, files in os.walk(f"interface/{st.session_state.username}", topdown = False):
-                for name in files:
-                    os.remove(os.path.join(root, name))
-                for name in dirs:
-                    os.rmdir(os.path.join(root, name))
-            os.rmdir(f"interface/{st.session_state.username}")
-            response = requests.post(f"{st.session_state.url}/terminate")
-            for key in st.session_state.keys():
-                del st.session_state[key]
             
-            st.rerun()
         
         if st.button("Delete Account"):
             auth.delete_user(st.session_state.username)
