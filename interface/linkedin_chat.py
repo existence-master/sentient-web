b1bb5767fe0656dc553b00a6811b6fa81e73b314
SK <sarthakkarandikar03@gmail.com>
2023-11-11 13:05:44 +0000

    Nov 11, 2023, 6:35 PM

--- interface/linkedin_chat.py
+++ interface/linkedin_chat.py
@@ -15,7 +15,7 @@ def app():
     
     with st.sidebar:
         
-        selected = option_menu("Main Menu", ["", 'Settings'], 
+        selected = option_menu("Main Menu", ["Casual Chat", 'Settings'], 
             icons=['house', 'gear'], menu_icon="cast", default_index=1)
         selected
 
