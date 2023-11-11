c6e2189c0243ee49474d76babff6109734140ca3
SK <sarthakkarandikar03@gmail.com>
2023-11-11 13:18:26 +0000

    Nov 11, 2023, 6:48 PM

--- interface/linkedin_chat.py
+++ interface/linkedin_chat.py
@@ -21,22 +21,26 @@ def app():
             if key == "Chat" :
                 st.session_state.runpage = casual_chat.app
                 st.rerun()
+
             elif key == "Settings" :
                 st.session_state.runpage = settings.app
                 st.rerun()
+
             elif key == "Logout" :
                 for root, dirs, files in os.walk(f"interface/{st.session_state.username}", topdown = False):
                     for name in files:
                         os.remove(os.path.join(root, name))
                     for name in dirs:
                         os.rmdir(os.path.join(root, name))
+                        
                 os.rmdir(f"interface/{st.session_state.username}")
                 requests.post(f"{st.session_state.url}/terminate")
-                
+
                 for key in st.session_state.keys():
                     del st.session_state[key]
                 
                 st.rerun()
+
             else :
                 pass
 
