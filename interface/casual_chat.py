fcd5d860cd710f2fe2b593193da19bc87b3f37e2
SK <sarthakkarandikar03@gmail.com>
2023-11-11 14:31:03 +0000

    Nov 11, 2023, 8:01 PM

--- interface/casual_chat.py
+++ interface/casual_chat.py
@@ -44,7 +44,20 @@ def app():
         
         st.title(f"Hello {st.session_state.username}")
         menu = option_menu(None, ["Chat", "LinkedIn Advice", "Settings", "Logout"], icons=["chat-fill", "linkedin", "gear", "box-arrow-in-left"], menu_icon = "cast", default_index = 0, on_change = on_page_change, key = "menu")
-        menu      
+        menu  
+
+        if st.button("Logout") :
+             for root, dirs, files in os.walk(f"interface/{st.session_state.username}", topdown = False):
+                    for name in files:
+                        os.remove(os.path.join(root, name))
+                    for name in dirs:
+                        os.rmdir(os.path.join(root, name))
+
+                os.rmdir(f"interface/{st.session_state.username}")
+                requests.post(f"{st.session_state.url}/terminate")
+
+                for key in st.session_state.keys():
+                    del st.session_state[key]    
 
        
     ai_container = st.container()
