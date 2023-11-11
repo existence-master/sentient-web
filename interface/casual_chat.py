5827714577314dd6a6345f80550ca4d7c2b59797
SK <sarthakkarandikar03@gmail.com>
2023-11-11 13:21:21 +0000

    Nov 11, 2023, 6:51 PM

--- interface/casual_chat.py
+++ interface/casual_chat.py
@@ -1,7 +1,7 @@
 import streamlit as st
 import os
 import requests
-from firebase_admin import auth
+
 
 def app():
     title_container = st.container()
@@ -42,21 +42,7 @@ def app():
 
         menu = option_menu(None, ["Chat", "LinkedIn Advice", "Settings", "Logout"], icons=["chat-fill", "linkedin", "gear", "box-arrow-in-left"], menu_icon = "cast", default_index = 0, on_change = on_page_change)
         menu      
-        if st.button("Logout"):
-            for root, dirs, files in os.walk(f"interface/{st.session_state.username}", topdown = False):
-                for name in files:
-                    os.remove(os.path.join(root, name))
-                for name in dirs:
-                    os.rmdir(os.path.join(root, name))
-            os.rmdir(f"interface/{st.session_state.username}")
-            response = requests.post(f"{st.session_state.url}/terminate")
-            for key in st.session_state.keys():
-                del st.session_state[key]
-            
-            st.rerun()
-        
        
-
     ai_container = st.container()
     user_container = st.container()
 
