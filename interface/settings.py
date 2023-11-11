7b63222b9e95961810790938c3eb14bfaf014907
SK <sarthakkarandikar03@gmail.com>
2023-11-11 14:36:36 +0000

    Nov 11, 2023, 8:06 PM

--- interface/settings.py
+++ interface/settings.py
@@ -21,7 +21,7 @@ def app() :
             else :
                 pass
 
-        menu = option_menu(None, ["Chat", "LinkedIn Advice", "Settings", "Logout"], icons=["chat-fill", "linkedin", "gear", "box-arrow-in-left"], menu_icon = "cast", default_index = 2, on_change = on_page_change, key = "menu")
+        menu = option_menu(None, ["Chat", "LinkedIn Advice", "Settings"], icons=["chat-fill", "linkedin", "gear"], menu_icon = "cast", default_index = 2, on_change = on_page_change, key = "menu")
         menu 
 
         if st.button("Logout") :
